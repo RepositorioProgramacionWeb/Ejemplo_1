825a0307a4001811f406e513982f5aa722dee37e
MateoAOA <125922660+MateoAOA@users.noreply.github.com>
2023-11-16 20:17:58 -0600

    q
    q

--- routes/web.php
+++ routes/web.php
@@ -15,3 +15,6 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', 'HomeController@index');
 Route::get('/practica', 'HomeController@practica');
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
